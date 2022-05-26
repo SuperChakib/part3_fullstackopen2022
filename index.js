89b8ee839605aef9caf55115572813f0169ba7c6
Chakib <mijikaihaikingu@tutanota.com>
2022-05-26 22:02:11 +0200

    part3 phonebook step3 allow display info single entry

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ app.get('/info', (request, response) => {
   response.send(message)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  person
+  ? response.json(person)
+  : response.sendStatus(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
