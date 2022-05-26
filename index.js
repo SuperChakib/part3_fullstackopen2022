002e8e0df3d51cea80a806892616d41919e3a036
Chakib <mijikaihaikingu@tutanota.com>
2022-05-26 21:45:03 +0200

    part3 phonebook step2 implement info page

--- index.js
+++ index.js
@@ -29,6 +29,12 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const message = `<p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date()}</p>`
+  response.send(message)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
