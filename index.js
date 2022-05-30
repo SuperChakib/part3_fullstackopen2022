af7289b0d2f211af759a372080b70df0bf56b929
Chakib <mijikaihaikingu@tutanota.com>
2022-05-30 11:10:41 +0200

    part3 phonebook step11 use production build of frontend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 morgan.token('data', (request, response) => request.method !== 'POST' ? '' : JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
